b9581dfb0750b277ff355861a37ca869ce5e5d46
danielschwendler <schwendler@amendllc.com>
2020-03-06 14:27:51 -0500

    Added Dan's Changes
    To be pasted by Dan into the master script as first portion of presentation

---
+++ Dan Changes.R
@@ -0,0 +1,5 @@
+
+# Dan's Additions ---------------------------------------------------------
+ggplot(diamonds) + geom_point(aes(x=carat, y=price, color=cut)) + geom_smooth(aes(x=carat, y=price, color=cut))
+
+#Comments for joe
