8445c3cb7421a5002b2f0e7ea25c2db724a24fa3
danielschwendler <schwendler@amendllc.com>
2020-03-06 14:36:56 -0500

    density plot
    Ad density plot to analysis

--- Dan Co-Dev Changes.R
+++ Dan Co-Dev Changes.R
@@ -1,5 +1,16 @@
 # Dan 2 Changes Lines 5 - 7-----------------------------------------------------------
 diamonds <- ggplot2::diamonds %>% #extract diamonds from package
   mutate(Carat_Price_Ratio = price/carat)
-  
+
 summary(diamonds) #print summary of diamonds to console
+
+
+
+# Carat_Price_Ratio -------------------------------------------------------
+
+p <- ggplot(diamonds, aes(x=price)) +
+  geom_density()
+p
+# Add mean line
+p+ geom_vline(aes(xintercept=mean(price)),
+              color="blue", linetype="dashed", size=1)
