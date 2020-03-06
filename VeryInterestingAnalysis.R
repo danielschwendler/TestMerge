414bdc5541768f6030b67ce6349bc9c11748fce7
danielschwendler <schwendler@amendllc.com>
2020-03-06 13:56:49 -0500

    Create VeryInterestingAnalysis.R

---
+++ VeryInterestingAnalysis.R
@@ -0,0 +1,34 @@
+# Load Data ---------------------------------------------------------------
+library(ggplot2) #load package ggplot for visuals and diamonds dataset
+library(dplyr)
+
+diamonds <- ggplot2::diamonds #extract diamonds from package
+
+summary(diamonds) #print summary of diamonds to console
+
+
+# EDA ---------------------------------------------------------------------
+
+diamonds %>%
+  summarise(mean = mean(price), sd = sd(price), median = median(price))
+
+diamonds %>%
+  group_by(cut) %>%
+  summarise(counts = n())
+
+diamonds %>%
+  group_by(cut) %>%
+  summarise(counts = n() / nrow(diamonds))
+
+
+# ggplots -----------------------------------------------------------------
+
+ggplot(data = diamonds, aes(x = color, fill = cut)) +
+  geom_bar()
+
+ggplot(data = diamonds, aes(x = depth, fill = cut)) +
+  geom_histogram(binwidth = 0.2)
+
+ggplot(data = diamonds, aes(x = depth)) +
+  geom_histogram(binwidth = 0.2) +
+  facet_wrap(~ cut)
