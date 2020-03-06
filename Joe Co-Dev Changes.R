43ce175bfa8beca568caed3fdc19e10318969497
danielschwendler <schwendler@amendllc.com>
2020-03-06 14:37:42 -0500

    Add changes for joe
    Added ratio and summary data frame for joe to add to full script

--- Joe Co-Dev Changes.R
+++ Joe Co-Dev Changes.R
@@ -1,4 +1,10 @@
+# Joe Changes Changes Lines 5 - 7-----------------------------------------------------------
+diamonds <- ggplot2::diamonds %>% #extract diamonds from package
+  mutate(Depth_Price_Ratio = price/depth)
 
-# Joe Changes -------------------------------------------------------------
 
 
+# Average price by color --------------------------------------------------
+
+diamonds %>% group_by(color) %>% summarize(Price = mean(price),
+                                           Depth_Price_Ratio = mean(Depth_Price_Ratio))
